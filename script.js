25e1594bdfddf0e3888be3ad7d679b3bb9625dfa
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-08-13 19:55:26 -0300

    andamento

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ const inputNome = document.querySelector("#nome");
 const inputSobrenome = document.querySelector("#sobrenome");
 const inputCpf = document.querySelector("#cpf");
 const inputEmail = document.querySelector("#email");
+
+const inputEditNome = document.querySelector("#edit-nome");
+const inputEditSobrenome = document.querySelector("#edit-sobrenome");
+const inputEditCpf = document.querySelector("#edit-cpf");
+const inputEditEmail = document.querySelector("#edit-email");
+
+const formEdicao = document.querySelector("#form-edicao");
+const btnCancelarEdicao = document.querySelector("#cancelar-edicao");
+const btnConfirmarExclusao = document.querySelector("#confirmar-exclusao");
+const btnCancelarExclusao = document.querySelector("#cancelar-exclusao");
+
+let clientes = [];
+let indexEditando = null;
+let indexExcluindo = null;
